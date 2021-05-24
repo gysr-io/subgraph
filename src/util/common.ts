068cf2f7a7173d5499e4821580c7039ae9a3cfb8
Iain McCown <iain.mccown@gmail.com>
2021-05-24 18:54:18 -0500

    Day data for pools

--- src/util/common.ts
+++ src/util/common.ts
@@ -2,7 +2,7 @@
 
 import { Address, BigDecimal, BigInt, log } from '@graphprotocol/graph-ts';
 
-import { Platform, User } from '../../generated/schema'
+import { Platform, User, PoolDayData, Geyser } from '../../generated/schema'
 import { ZERO_BIG_INT, ZERO_BIG_DECIMAL, ZERO_ADDRESS } from '../util/constants'
 
 
@@ -30,4 +30,26 @@ export function createNewPlatform(): Platform {
   platform._geysers = [];
 
   return platform;
+}
+
+export function updatePoolDayData(geyser: Geyser, timestamp: number): PoolDayData {
+  let day = Math.floor(timestamp / 86400) as i32;
+  let dayStartTimestamp = day * 86400; // will be 12:00am UTC due to rounding
+  let id = geyser.id + '_' + day.toString();
+
+  let poolDayData = PoolDayData.load(id);
+  if (poolDayData === null) {
+    poolDayData = new PoolDayData(id);
+    poolDayData.poolAddress = geyser.id;
+    poolDayData.date = dayStartTimestamp;
+    poolDayData.volume = ZERO_BIG_DECIMAL;
+  }
+
+  poolDayData.totalStaked = geyser.staked;
+  poolDayData.totalGysrSpent = geyser.gysrSpent;
+  poolDayData.totalUsers = geyser.users;
+  poolDayData.tvl = geyser.tvl;
+  poolDayData.volume = ZERO_BIG_DECIMAL;
+
+  return poolDayData!;
 }
