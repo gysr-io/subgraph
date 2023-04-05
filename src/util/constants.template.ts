bb69d1c3e2df60a943533747d780affd5eacf72b
Devin A. Conley <devinaconley@gmail.com>
2023-04-05 02:01:06 -0400

    remove temporary module factories from config

--- src/util/constants.template.ts
+++ src/util/constants.template.ts
@@ -51,40 +51,37 @@ export let INITIAL_SHARES_PER_TOKEN = BigDecimal.fromString('1000000');
 
 // factories
 export let ASSIGNMENT_STAKING_MODULE_FACTORIES: Address[] = [
-  Address.fromString('{{assignment_staking_module_factory_address}}'),
-  Address.fromString('{{assignment_staking_module_factory_address_v3beta}}')
+  Address.fromString('{{assignment_staking_module_factory_address}}')
 ];
 export let ERC20_COMPETITIVE_REWARD_MODULE_FACTORIES_V2: Address[] = [
   Address.fromString('{{erc20_competitive_reward_module_factory_address}}'),
   Address.fromString('{{erc20_competitive_reward_module_factory_address_v211}}')
 ];
 export let ERC20_COMPETITIVE_REWARD_MODULE_FACTORIES_V3: Address[] = [
-  Address.fromString('{{erc20_competitive_reward_module_factory_address_v3}}'),
-  Address.fromString('{{erc20_competitive_reward_module_factory_address_v3beta}}')
+  Address.fromString('{{erc20_competitive_reward_module_factory_address_v3a}}'),
+  Address.fromString('{{erc20_competitive_reward_module_factory_address_v3}}')
 ];
 export let ERC20_FRIENDLY_REWARD_MODULE_FACTORIES_V2: Address[] = [
   Address.fromString('{{erc20_friendly_reward_module_factory_address}}'),
   Address.fromString('{{erc20_friendly_reward_module_factory_address_v211}}')
 ];
 export let ERC20_FRIENDLY_REWARD_MODULE_FACTORIES_V3: Address[] = [
-  Address.fromString('{{erc20_friendly_reward_module_factory_address_v3}}'),
-  Address.fromString('{{erc20_friendly_reward_module_factory_address_v3beta}}')
+  Address.fromString('{{erc20_friendly_reward_module_factory_address_v3a}}'),
+  Address.fromString('{{erc20_friendly_reward_module_factory_address_v3}}')
 ];
 export let ERC20_LINEAR_REWARD_MODULE_FACTORIES: Address[] = [
-  Address.fromString('{{erc20_linear_reward_module_factory_address}}'),
-  Address.fromString('{{erc20_linear_reward_module_factory_address_v3beta}}')
+  Address.fromString('{{erc20_linear_reward_module_factory_address}}')
 ];
 export let ERC20_MULTI_REWARD_MODULE_FACTORIES: Address[] = [
-  Address.fromString('{{erc20_multi_reward_module_factory_address}}'),
-  Address.fromString('{{erc20_multi_reward_module_factory_address_v3beta}}')
+  Address.fromString('{{erc20_multi_reward_module_factory_address}}')
 ];
 export let ERC20_STAKING_MODULE_FACTORIES: Address[] = [
   Address.fromString('{{erc20_staking_module_factory_address}}'),
-  Address.fromString('{{erc20_staking_module_factory_address_v3beta}}')
+  Address.fromString('{{erc20_staking_module_factory_address_v3}}')
 ];
 export let ERC721_STAKING_MODULE_FACTORIES: Address[] = [
   Address.fromString('{{erc721_staking_module_factory_address}}'),
-  Address.fromString('{{erc721_staking_module_factory_address_v3beta}}')
+  Address.fromString('{{erc721_staking_module_factory_address_v3}}')
 ];
 
 // types
